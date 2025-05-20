784b32c4b7311bf2141a05ba6631c9b2fae340c0
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-20 18:14:57 +0200

    add content main

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+export default function Content() {
+  return (
+    <section className="content-sec">
+      <div className="container">
+        <h1 className="title-content">-- Content goes here --</h1>
+      </div>
+    </section>
+  );
+}
