d681d78164b31aa8dbdf75a6573cba54d92ac6ae
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-21 16:26:23 +0200

    add terms of the site

--- src/components/Merchandise.jsx
+++ src/components/Merchandise.jsx
@@ -2,7 +2,7 @@ export default function Merchandise() {
   return (
     <div className="background-blue">
       <div className="container">
-        <div className="merchandise-sec">
+        <div id="merchandise-sec">
           <figure>
             <img
               className="logo-merchandise"
@@ -12,36 +12,24 @@ export default function Merchandise() {
             <span>DIGITAL COMICS</span>
           </figure>
           <figure>
-            <img
-              className="logo-merchandise"
-              src="/digital-comics.png"
-              alt=""
-            />
-            <span>DIGITAL COMICS</span>
+            <img className="logo-merchandise" src="/merchandise.png" alt="" />
+            <span>DC MERCHANDISE</span>
           </figure>
           <figure>
             <img
               className="logo-merchandise"
-              src="/digital-comics.png"
+              src="/comics-subscription.png"
               alt=""
             />
-            <span>DIGITAL COMICS</span>
+            <span>SUBSCRIPTION</span>
           </figure>
           <figure>
-            <img
-              className="logo-merchandise"
-              src="/digital-comics.png"
-              alt=""
-            />
-            <span>DIGITAL COMICS</span>
+            <img className="logo-merchandise" src="/comics-shop.png" alt="" />
+            <span> COMICS SHOP LAVORATOR</span>
           </figure>
           <figure>
-            <img
-              className="logo-merchandise"
-              src="/digital-comics.png"
-              alt=""
-            />
-            <span>DIGITAL COMICS</span>
+            <img className="logo-merchandise" src="/power-visa.svg" alt="" />
+            <span>DC POWER VISA</span>
           </figure>
         </div>
       </div>
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,11 +1,13 @@
 import Content from "./Content";
 import Merchandise from "./Merchandise";
+import Termsofthesite from "./Termsofthesite";
 
 export default function Main() {
   return (
     <>
       <Content />
       <Merchandise />
+      <Termsofthesite />
     </>
   );
 }
