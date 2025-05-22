47464fa494b0fac3f57b09eda226c716c0ae716a
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-05-22 17:36:32 +0200

    add-css-card

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,11 +3,12 @@ import ComicsCards from "./ComicsCards";
 export default function Content() {
   return (
     <>
-      <ComicsCards />
       <div className="background-black">
         <div className="container">
           <section className="content-sec">
-            <h1 className="title-content">-- Content goes here --</h1>
+            <h1 className="title-content">
+              <ComicsCards />
+            </h1>
           </section>
         </div>
       </div>
